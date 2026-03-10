0ba30e985647c230b62a4b474689bb18e481af14
Ejaz47-Hub <ejaz47026@gmail.com>
2026-03-10 13:08:35 +0530

    Nodejs

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 import express from "express";
 
 const app = express()
+
+
+app.get("/",(req,res)=>{
+    res.send("Hey")
+})
+
+app.get("/about",(req,res)=>{
+    res.send("Hello World")
+})
+
 app.listen(3000, ()=>{
     console.log("server is running");
-})
+})  
