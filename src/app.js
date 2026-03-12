d824d547581c71717004f1405ded9454d9c4e85d
Ejaz47-Hub <ejaz47026@gmail.com>
2026-03-12 11:39:09 +0530

    Nodejs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,47 @@
 import express from "express";
+import noteModel from "../models/node.model.js";
 
 const app = express()
 app.use(express.json())
+
+app.post("/notes",async(req,res)=>{
+   const data =  req.body
+ await noteModel.create({
+    title : data.title,
+    description : data.description
+   })
+   res.status(201).json({
+    message : "Note created"
+   })
+})
+
+app.get("/notes",async(req,res)=>{
+  const notes =  await noteModel.find()
+  res.status(200).json({
+    message : "Notes Fetched",
+    notes : notes
+  })
+})
+
+
+ app.delete("/notes/:id", async(req,res)=>{
+    const id = req.params.id 
+    await noteModel.findOneAndDelete({
+        _id : id
+    })
+    res.status(200).json({
+        message : "Note Deleted"
+    })
+ })
+
+ app.patch("/notes/:id",async(req,res)=>{
+    const id = req.params.id
+    const description = req.body.description
+    await noteModel.findOneAndUpdate({_id:id },{description:description})
+    res.status(200).json({
+        message : "Note Updated"
+    })
+ })
 
 
 export default app
